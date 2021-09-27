66e573bbc98c336caa1b8a02714ede8787fc8665
Nagah <E-n.Shinawy@lean.sa>
2021-09-27 18:51:06 +0300

    OR query , age__lte

--- apps/core/shell/student.py
+++ apps/core/shell/student.py
@@ -1,8 +1,12 @@
+from django.db.models import Q
 from apps.core.models import Student
 
 students = Student.objects.all()
 
 print(students)
+
+####################################################################################################
+
 
 print("#" * 100)
 males = Student.objects.get_by_males()
@@ -11,8 +15,42 @@ print(males)
 
 print("#" * 100)
 
+
+####################################################################################################
+
+
 females = Student.objects.get_by_females()
 
 print(females)
+
+print("#" * 100)
+
+####################################################################################################
+
+# OR query
+
+qs = Student.objects.filter(
+    Q(firstname__istartswith="j") | Q(firstname__istartswith="a")
+)
+
+print(qs)
+
+print("#" * 100)
+
+####################################################################################################
+
+qs = Student.objects.filter(age__in=[23, 25])
+
+
+print(qs)
+
+print("#" * 100)
+
+####################################################################################################
+
+
+qs = Student.objects.filter(age__lte=30)
+
+print(qs)
 
 # from apps.core.shell.student import *
