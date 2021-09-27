f777242076ec7acc32f2bb5e742e180ea46719c2
Nagah <E-n.Shinawy@lean.sa>
2021-09-27 18:57:16 +0300

    more qs with Student

--- apps/core/managers/student.py
+++ apps/core/managers/student.py
@@ -4,6 +4,8 @@ from apps.core.choices import Gender
 
 class StudentManager(models.Manager):
 
+    UNDER_AGE = 18
+
     def get_by_firstname(self, firstname):
         return self.filter(firstname=firstname)
 
@@ -12,6 +14,9 @@ class StudentManager(models.Manager):
 
     def get_by_age_rage(self, start, end):
         return self.filter(age__range=[start, end])
+
+    def get_under_age(self):
+        return self.filter(age__lt=self.UNDER_AGE)
 
     def get_by_males(self):
         return self.filter(gender=Gender.male)
--- apps/core/shell/student.py
+++ apps/core/shell/student.py
@@ -1,6 +1,8 @@
 from django.db.models import Q
 from apps.core.models import Student
 
+UNDER_AGE = 18
+
 students = Student.objects.all()
 
 print(students)
@@ -52,5 +54,22 @@ print("#" * 100)
 qs = Student.objects.filter(age__lte=30)
 
 print(qs)
+
+print("#" * 100)
+
+####################################################################################################
+
+qs = Student.objects.get_by_age(age=25)
+
+
+print(qs)
+
+print("#" * 100)
+
+####################################################################################################
+
+under_age = Student.objects.get_under_age()
+print(under_age)
+
 
 # from apps.core.shell.student import *
