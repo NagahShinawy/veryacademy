9ec4b82792cfe5346db8fb58f20ef6262694b338
Nagah <E-n.Shinawy@lean.sa>
2021-09-29 10:29:01 +0300

    custom error message for invalid age range

--- apps/core/validators/base_validation.py
+++ apps/core/validators/base_validation.py
@@ -9,9 +9,14 @@ class RangeValidator(MinValueValidator):
         if msg:
             self.message = msg
         else:
-            self.message = f"Range must in between {limit_value}-{max_value}"
+            self.message = "Range must in between {limit_value}-{max_value} not {value}"
 
     def clean(self, x):
-        if x not in range(self.limit_value, self.max_value + 1):
-            raise ValidationError(self.message)
-        return x
+        value = super().clean(x)
+        if value not in range(self.limit_value, self.max_value + 1):
+            raise ValidationError(
+                self.message.format(
+                    limit_value=self.limit_value, max_value=self.max_value, value=value
+                )
+            )
+        return value
