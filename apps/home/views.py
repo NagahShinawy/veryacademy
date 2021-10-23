a79d5426d346a2e866505ffc0735915c2b3dcde4
Nagah <E-n.Shinawy@lean.sa>
2021-10-23 11:04:48 +0300

    adding custom user context

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,12 +2,15 @@ from django.views.generic.base import TemplateView
 
 
 class NoteIndexView(TemplateView):
+    # django template language DTL: allow you to use vars, tags and filters at HTML pages
+
+    # source : https://realpython.com/django-templates-tags-filters/#choosing-a-template-language
     template_name = "home/home.html"
-    DEFAULT_USER = "lovely user"
+    DEFAULT_USER = "Unknown"
 
     def get_context_data(self, **kwargs):
         data = super(NoteIndexView, self).get_context_data(**kwargs)
-        data["welcome"] = (
+        data["custom_user"] = (
             self.request.user.username.title()
             if self.request.user.is_authenticated
             else self.DEFAULT_USER
