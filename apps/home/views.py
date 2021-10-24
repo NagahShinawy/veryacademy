6e27278f627caea5eb4f1ad60618e25b50848d51
Nagah <E-n.Shinawy@lean.sa>
2021-10-24 10:11:19 +0300

    refactor

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,16 +3,15 @@ from django.views.generic.base import TemplateView
 
 class NoteIndexView(TemplateView):
     # django template language DTL: allow you to use vars, tags and filters at HTML pages
-
     # source : https://realpython.com/django-templates-tags-filters/#choosing-a-template-language
     template_name = "home/home.html"
-    DEFAULT_USER = "Unknown"
+    UNKNOWN_USER = "Unknown"
 
     def get_context_data(self, **kwargs):
         data = super(NoteIndexView, self).get_context_data(**kwargs)
         data["custom_user"] = (
             self.request.user.username.title()
             if self.request.user.is_authenticated
-            else self.DEFAULT_USER
+            else self.UNKNOWN_USER
         )
         return data
