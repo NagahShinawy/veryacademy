b2322f5ff3806e07facc9115debc8fac151e0bba
Nagah <E-n.Shinawy@lean.sa>
2021-10-25 15:57:11 +0300

    read only for note model

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -7,3 +7,12 @@ from apps.home.models import Note
 class NoteModelAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "description", "created", "updated", "is_active")
     list_editable = ("title", "description", "is_active")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
