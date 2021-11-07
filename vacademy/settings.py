cac8dc220b69f290933eb0bb44794d79a7d1cced
Nagah <E-n.Shinawy@lean.sa>
2021-11-07 08:46:22 +0300

    adding style config

--- vacademy/settings.py
+++ vacademy/settings.py
@@ -140,8 +140,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+BASE_STATIC_FILE = BASE_DIR / "static",
+MASTERCLASS_STATIC_FILE = BASE_DIR / 'apps/masterclass/static'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    BASE_DIR / 'apps/masterclass/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
