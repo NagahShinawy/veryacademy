926f4f7f79121c8844286848968e6d5150218aba
Nagah <E-n.Shinawy@lean.sa>
2021-11-07 06:51:23 +0300

    get single book with detail view

--- apps/masterclass/urls.py
+++ apps/masterclass/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import books
+from .views import books, book_details
 
 
 app_name = "masterclass"
 
 urlpatterns = [
     path("", books, name="books"),
+    path("books/<int:pk>", book_details, name="book_details"),
 ]
--- apps/masterclass/views.py
+++ apps/masterclass/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, Http404
 from .models import Book
 
 
 def books(request):
     return render(request, "masterclass/books.html", {"books": Book.objects.all()})
 
+
+def book_details(request, pk):
+    try:
+        book = Book.objects.get(pk=pk)
+    
+    except ObjectDoesNotExist:
+        raise Http404
+    return render(request, "masterclass/book.html", {"book": book})
